7f9dc19a6ab01349c84d07a3d5afd59db5dfe9e7
WannandyS <161842255+WannandyS@users.noreply.github.com>
2025-05-14 19:34:51 +0700

    update

--- src/main/java/com/mycompany/mavenproject3/ProductService.java
+++ src/main/java/com/mycompany/mavenproject3/ProductService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.mycompany.mavenproject3;
 
 import java.util.ArrayList;
@@ -33,40 +32,4 @@ public class ProductService {
     public boolean delete(int id) {
         return productMap.remove(id) != null;
     }
-=======
-package com.mycompany.mavenproject3;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class ProductService {
-    private final Map<Integer, Product> productMap = new HashMap<>();
-    private int nextId = 0;
-
-    public List<Product> getAll() {
-        return new ArrayList<>(productMap.values());
-    }
-
-    public Product get(int id) {
-        return productMap.get(id);
-    }
-
-    public Product add(Product product) {
-        product.setId(nextId);
-        productMap.put(product.getId(), product);
-        return product;
-    }
-
-    public Product update(int id, Product product) {
-        product.setId(id);
-        productMap.put(id, product);
-        return product;
-    }
-
-    public boolean delete(int id) {
-        return productMap.remove(id) != null;
-    }
->>>>>>> 046f2788f15340475b23bbbe39419f6a3e06b857
 }
